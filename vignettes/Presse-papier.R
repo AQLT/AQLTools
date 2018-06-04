f3c92ad182bad7e877deed4c26256677a9ec6445
AQLT <alain.quartier@yahoo.fr>
2018-06-04 10:46:56 +0200

    update

--- vignettes/Presse-papier.R
+++ vignettes/Presse-papier.R
@@ -3,16 +3,7 @@ knitr::opts_chunk$set(echo = TRUE, size = "small")
 
 ## ----echo=FALSE----------------------------------------------------------
 require(AQLTools,quietly = T)
-require(DT,quietly = T)
-options(OutDec= ",",
-        DT.options = list(dom = 'Bt',
-                          buttons = list(list(
-                                             extend = 'excel',
-                                             text = 'Télécharger tableau'
-                                         )),
-                          language=list(decimal=",")
-        )
-)
+options(OutDec= ",")
 data<-matrix(c("Indicateur synthétique","100","106","108","109","108","Production passée","5","9","17","14","10","Stocks","13","7","6","5","6","Carnets de commandes globaux","-18","-9","-6","-5","-3","Carnets de commandes étrangers","-14","-5","-1","-2","-2","Perspectives personnelles de production","5","11","12","16","13","Perspectives générales de production","-9","3","1","7","17"),
           nrow=7,byrow = T)
 data<-data.frame(data,stringsAsFactors = F)
