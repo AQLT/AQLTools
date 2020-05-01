797f7214913c6d4cc43a1267504be999cbdd0f21
AQLT <alain.quartier@yahoo.fr>
2020-05-01 12:07:59 +0200

    compatibilité séries annuelles + séparateur universel

--- R/ts_utils.R
+++ R/ts_utils.R
@@ -99,8 +99,7 @@ volatilite.xts <- function(x, lag = 1){
 #'
 #' @param x table contenant au moins deux colonnes dont une avec des dates.
 #' @param col_date numéro ou nom de la colonne contenant les dates (par défaut c'est la première colonne).
-#' @param sep_date caractère utilisé pour séparé le jour, le mois et l'année (par défaut \code{col_date = "/"}
-#' c'est-à-dire que les dates sont au format JJ/MM/AAAA ou AAAA/MM/JJ).
+#' @param sep_date caractère utilisé pour séparé le jour, le mois et l'année. Détection automatique par défaut.
 #' @param frequence fréquence de l'objet \code{\link[stats]{ts}}. Par défaut la fonction fait une recherche automatique
 #' (il peut y avoir des problèmes si l'on n'a pas d'année entière connue).
 #'
@@ -113,49 +112,63 @@ volatilite.xts <- function(x, lag = 1){
 #' @encoding UTF-8
 #' @examples
 #' x <- data.frame(dates=paste(rep(2000:2002, each=4),
-#' rep(c("01", "04", "07", "10"),2) ,"01", sep = "-"),
-#' donnees = 1:12, stringsAsFactors = FALSE)
+#'                             rep(c("01", "04", "07", "10"),2) ,"01", sep = "-"),
+#'                 donnees = 1:12, stringsAsFactors = FALSE)
 #' ymd_ts(x)
 #' @name data_to_ts
 #' @rdname data_to_ts
 #' @export
-ymd_ts <- function(x, col_date = 1,sep_date = "/", frequence = NULL){
+ymd_ts <- function(x, col_date = 1, sep_date, frequence = NULL){
     if (is.character(col_date)){
         col_date <- which(names(x)==col_date)
     }
-
+    if(missing(sep_date)){
+        sep_date <- "[^0-9.]"
+    }
     dates <- x[,col_date]
-    dates <- gsub(sep_date, "", dates, fixed = TRUE)
+    dates <- gsub(sep_date, "", dates)
 
     if ( is.null(frequence) ){#On cherche la fréquence
         #On regarde le nombre maximal d'occurence de chaque année
         frequence <- max(table(substr(dates,1,4)))
     }
 
-    prem_date <- as.numeric(c(substr(dates[1],1,4),substr(dates[1],5,6)))
-    prem_date <- prem_date[1] + (prem_date[2]-1)/frequence
+    if(nchar(dates)>4){
+        prem_date <- as.numeric(c(substr(dates[1],1,4),substr(dates[1],5,6)))
+        prem_date <- prem_date[1] + (prem_date[2]-1)/frequence
+    }else{
+        # On n'identifie que l'année
+        prem_date <- as.numeric(substr(dates[1],1,4))
+    }
+
 
     data_hors_dates <- x[,-col_date]
     return(ts(data_hors_dates, start = prem_date, frequency = frequence))
 }
 #' @rdname data_to_ts
 #' @export
-dmy_ts <- function(x, col_date = 1,sep_date = "/", frequence = NULL){
+dmy_ts <- function(x, col_date = 1,sep_date, frequence = NULL){
     if (is.character(col_date)){
         col_date <- which(names(x)==col_date)
     }
-
+    if(missing(sep_date)){
+        sep_date <- "[^0-9.]"
+    }
     dates <- x[,col_date]
-    dates <- gsub(sep_date, "", dates, fixed = TRUE)
+    dates <- gsub(sep_date, "", dates)
 
     if ( is.null(frequence) ){#On cherche la fréquence
         #On regarde le nombre maximal d'occurence de chaque année
         frequence <- max(table(substr(dates,5,8)))
     }
 
-    prem_date <- as.numeric(c(substr(dates[1],5,8),substr(dates[1],3,4)))
-    prem_date <- prem_date[1] + (prem_date[2]-1)/frequence
-
+    if(nchar(dates)>4){
+        prem_date <- as.numeric(c(substr(dates[1],5,8),substr(dates[1],3,4)))
+        prem_date <- prem_date[1] + (prem_date[2]-1)/frequence
+    }else{
+        # On n'identifie que l'année
+        prem_date <- as.numeric(substr(dates[1],1,4))
+    }
     data_hors_dates <- x[,-col_date]
     return(ts(data_hors_dates, start = prem_date, frequency = frequence))
 }
